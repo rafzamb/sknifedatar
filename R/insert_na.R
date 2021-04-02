cd98b8340fe35d1a9935833d3ab24168617a1587
rafaelzambrano1992 <rafael.zambrano@uala.com.ar>
2021-04-01 23:03:00 -0300

    upadate 1

--- R/insert_na.R
+++ R/insert_na.R
@@ -1,30 +1,30 @@
-#' @title Función para agregar valores NA a un data frame
+#' @title Function para agregar valores NA a un data frame
 #'
 #' @description Esta función permite agregar valores NA a un data frame, pudiendo seleccionar las columnas
 #'    y la propòrcion de NAs deseados.
 #'
-#' @param data data frame
-#' @param columnas vector que indica el nombre de las columnas donde se agregaran los valores NA, ejemplo de formato c("X1","X2").
-#' @param p valor entre 0 y 1, indica la proporción de valores NA que se agregaran.
+#' @param .dataset data frame
+#' @param columns vector que indica el nombre de las columnas donde se agregaran los valores NA, ejemplo de formato c("X1","X2").
+#' @param .p valor entre 0 y 1, indica la proporción de valores NA que se agregaran.
 #' @param seed semilla de números aleatorios.
 #'
 #' @return El data frame original suministrado a data, pero con los valores NA agregados en las columnas indicadas.
 #' @export
 #'
 #' @examples
-#' insert_na(data = iris, columnas = c("Sepal.Length","Petal.Length"), p = 0.25)
-insert_na <- function(data , columnas, p = 0.01 , seed = 123){
+#' insert_na(.dataset = iris, columns = c("Sepal.Length","Petal.Length"), .p = 0.25)
+insert_na <- function(.dataset , columns, .p = 0.01 , seed = 123){
 
   set.seed(seed)
-  q <- 1-p
+  q <- 1-.p
 
-  data_na <- purrr::map_df(data %>%  dplyr::select(columnas), function(x) {
+  data_na <- purrr::map_df(.dataset %>%  dplyr::select(columns), function(x) {
     x[sample(c(TRUE, NA),
-             prob = c(q, p),
+             prob = c(q, .p),
              size = length(x),
              replace = TRUE)]})
 
-  dplyr::bind_cols(data %>% dplyr::select(-columnas),
+  dplyr::bind_cols(.dataset %>% dplyr::select(-columns),
                    data_na) %>%
     tibble::as_tibble()
 
--- R/multieval.R
+++ R/multieval.R
@@ -4,12 +4,11 @@
 #'
 #' @seealso \href{https://rafael-zambrano-blog-ds.netlify.app/posts/2020-12-22-prediccin-de-delitos-en-caba/#modelos-de-series-de-tiempo}{Predicción de delitos/multieval}
 #'
-#' @param data data frame con las predicciones, debe tener como mínimo la columna con los datos observados y al menos una columna que haga referencia a las predicciones de un modelo.
-#' @param observed string con el nombre de la columna que contiene los datos observados.
-#' @param predictions string o vector de strings las columnas donde se almacenan las predicicones.
-#' @param metrica métrica o conjunto de métricas que se desean evaluar, las métricas hacen referencias a las permitidas por
+#' @param .dataset data frame con las predicciones, debe tener como mínimo la columna con los datos observados y al menos una columna que haga referencia a las predicciones de un modelo.
+#' @param .observed string con el nombre de la columna que contiene los datos observados.
+#' @param .predictions string o vector de strings las columnas donde se almacenan las predicicones.
+#' @param .metrics métrica o conjunto de métricas que se desean evaluar, las métricas hacen referencias a las permitidas por
 #'   el paquete \href{https://yardstick.tidymodels.org/}{yardstick} de tidymodels.
-#' @param plot_view TRUE para hacer un gráfico de las métricas resultantes.
 #' @param value_table TRUE para mostrar las métricas desagregadas.
 #'
 #' @importFrom rlang .data
@@ -18,55 +17,43 @@
 #' @export
 #'
 #' @example man/examples/multieval_example.R
-multieval <- function(data , observed, predictions, metrica ,plot_view = TRUE, value_table = TRUE){
-
-  names(predictions) <- predictions
-
-  table_values <- mlapply(function(x, y ){
-
-    x(data = data,
-      truth    = .data[[observed]],
-      estimate = .data[[y]],
-      na_rm    = TRUE) %>%
-      dplyr::mutate(model = y)
-  },
-  metrica,predictions
-  ) %>%
-    dplyr::bind_rows() %>%
-    dplyr::arrange(.data$.metric)
-
+multieval = function(.dataset , .observed, .predictions, .metrics, value_table = FALSE){
+  
+  fucntion_aux = function(.dataset , .observed, .predictions, .metrics){
+    
+    names(.predictions) = .predictions
+    
+    purrr::map2(.metrics, .predictions, function(x = .metrics, y = .predictions) {
+      
+      x(data = .dataset,
+        truth = .dataset[[.observed]],
+        estimate = .dataset[[y]],
+        na_rm = TRUE) %>% 
+        dplyr::mutate(modelo = y) 
+    }) %>% 
+      dplyr::bind_rows()
+  }
+  
+  list_metrics = list()
+  
+  for (i in 1:length(.metrics)) {
+    
+    list_metrics[[i]] = fucntion_aux(.dataset  , .observed, .predictions, .metrics = .metrics[i])
+  }
+  
+  table_values <- list_metrics %>% dplyr::bind_rows()
+  
   summary_table <-
     table_values %>%
     dplyr::select(-".estimator") %>%
     tidyr::pivot_wider(names_from = .data$.metric, values_from = .data$.estimate)
-
-  if(plot_view == "TRUE"){
-
-    plot_metrics <- table_values %>%
-      ggplot2::ggplot(ggplot2::aes(x= .data$model, y= .data$.estimate)) +
-      ggplot2::geom_segment(ggplot2::aes(x=.data$model, xend=.data$model, y=0, yend=.data$.estimate, color = .data$model)) +
-      ggplot2::geom_point(size=3, color="red", alpha=0.5, shape=21, stroke=1) +
-      ggplot2::coord_flip()+
-      ggplot2::xlab("") +
-      ggplot2::facet_wrap(~ .metric, nrow = 1, scales = "free") +
-      ggplot2::theme_minimal() +
-      ggplot2::theme(axis.text.y = ggplot2::element_blank(),
-                     axis.text.x = ggplot2::element_text(angle = 90, vjust = 0.5, hjust=1),
-                     strip.text = ggplot2::element_text(face = "bold", size = 10)) +
-      ggplot2::scale_y_continuous(label= scales::comma)
-
-  }else{
-
-    plot_metrics = NULL
-  }
-
+  
   if(value_table == FALSE){
-
+    
     table_values = NULL
   }
-
+  
   list(table_values = table_values,
-       summary_table = summary_table,
-       plot_metrics =  plot_metrics) %>%
+       summary_table = summary_table) %>%
     purrr::compact()
 }
