2abe8cbc671b4d48716ad78477945b16e1e1a5e0
karbartolome <karinabartolome@hotmail.com>
2021-04-22 00:57:05 -0300

    modeltime workflowsets heatmap
    Function to generate a heatmap plot from a wffits object generated with modeltime_wfs_fit

--- R/globals.R
+++ R/globals.R
@@ -1,1 +1,1 @@
-utils::globalVariables(c(".","Name","nested_model","nested_column",".model_desc","calibration", ".model_id","best_model", "wflow_id", "list_models", ".fit_model"))
+utils::globalVariables(c(".","Name","nested_model","nested_column",".model_desc","calibration", ".model_id","best_model", "wflow_id", "list_models", ".fit_model", "na_text","metric_color"))
